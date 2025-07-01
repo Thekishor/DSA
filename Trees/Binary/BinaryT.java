10e557a0b97603be2b62c0884303a5182529c726
Kishor <kishorpandey2121@gmail.com>
2025-07-01 13:39:40 +0545

    binary search preorder

--- Trees/Binary/BinaryT.java
+++ Trees/Binary/BinaryT.java
@@ -30,9 +30,19 @@ public class BinaryT {
     }
   }
 
+  public static void preOrder(Node root) {
+    if (root == null) {
+      return;
+    }
+    System.out.print(root.data + " ");
+    preOrder(root.left);
+    preOrder(root.right);
+  }
+
   public static void main(String[] args) {
     int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     Node root = BinaryTree.buildTree(nodes);
-    System.out.println(root.data);
+    // System.out.println(root.data);
+    preOrder(root);
   }
 }

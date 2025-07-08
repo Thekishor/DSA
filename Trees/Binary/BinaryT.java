5d40c8279448ebb55f23378047c9178d4490441f
Kishor <kishorpandey2121@gmail.com>
2025-07-08 17:09:09 +0545

    diameter of binary tree

--- Trees/Binary/BinaryT.java
+++ Trees/Binary/BinaryT.java
@@ -59,6 +59,76 @@ public class BinaryT {
     System.out.print(root.data + " ");
   }
 
+  public static int countNodes(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftNodeCount = countNodes(root.left);
+    int rightNodeCount = countNodes(root.right);
+    return leftNodeCount + rightNodeCount + 1;
+  }
+
+  public static int heightOfTree(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftNodeHeight = heightOfTree(root.left);
+    int rightNodeHeight = heightOfTree(root.right);
+
+    int heightOfTreeNode = Math.max(leftNodeHeight, rightNodeHeight) + 1;
+    return heightOfTreeNode;
+  }
+
+  public static int diameter(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    int diam1 = diameter(root.left);
+    int diam2 = diameter(root.right);
+    int diam3 = heightOfTree(root.left) + heightOfTree(root.right) + 1;
+
+    return Math.max(diam3, Math.max(diam1, diam2));
+  }
+
+  public static int sumOfNodes(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftNodeSum = sumOfNodes(root.left);
+    int rightNodeSum = sumOfNodes(root.right);
+    return leftNodeSum + rightNodeSum + root.data;
+  }
+
+  static class TreeInfo {
+    int height;
+    int diameter;
+
+    TreeInfo(int height, int diameter) {
+      this.height = height;
+      this.diameter = diameter;
+    }
+  }
+
+  public static TreeInfo diameterTree(Node root) {
+
+    if (root == null) {
+      return new TreeInfo(0, 0);
+    }
+    TreeInfo left = diameterTree(root.left);
+    TreeInfo right = diameterTree(root.right);
+
+    int heightOfLeftAndRigth = Math.max(left.height, right.height) + 1;
+
+    int diam1 = left.diameter;
+    int diam2 = right.diameter;
+    int diam3 = left.height + right.height + 1;
+
+    int diameterTree = Math.max(Math.max(diam1, diam2), diam3);
+
+    TreeInfo treeInfo = new TreeInfo(heightOfLeftAndRigth, diameterTree);
+    return treeInfo;
+  }
+
   public static void main(String[] args) {
     int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     Node root = BinaryTree.buildTree(nodes);
@@ -75,5 +145,21 @@ public class BinaryT {
 
     System.out.println("PostOrder Data");
     postOrder(root);
+
+    System.out.println();
+
+    int count = countNodes(root);
+    System.out.println("Total number of nodes is : " + count);
+
+    int sumOfTreeNode = sumOfNodes(root);
+    System.out.println("Sum of nodes is : " + sumOfTreeNode);
+
+    int heigthOfTreeNode = heightOfTree(root);
+    System.out.println("The height of the tree is : " + heigthOfTreeNode);
+
+    int diameterOfTree = diameter(root);
+    System.out.println("Diameter of the binary tree is : " + diameterOfTree);
+
+    System.out.println("Diameter of the binary tree using approach 2 : " + diameterTree(root).diameter);
   }
 }
